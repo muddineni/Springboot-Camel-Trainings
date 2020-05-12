81366630a785d30cb6d9e7d912e4a54cbfec993a
Sampath Muddineni <sampath.muddineni@inmarsat.com>
2020-05-12 23:29:16 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/EnrichAndPollEnrichRoute.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnrichAndPollEnrichRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("timer://enrich?period=10000").routeId("enrich").autoStartup(true).setBody(constant("goodmornign"))
+				.enrich("file://enrichfolder/?fileName=enrich.txt")
+
+				.end();
+
+		from("timer://pollenrich?period=10000").routeId("enrich").autoStartup(true).log("beofre pollenrich::${body}")
+				.pollEnrich("file://enrichfolder/?fileName=enrich.txt", 10000).log("after pollenrich::${body}").end();
+	}
+}
